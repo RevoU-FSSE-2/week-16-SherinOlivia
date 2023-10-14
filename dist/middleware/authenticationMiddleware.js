22bee3db55fbf3b1f3303f4c9efa9ac9c4436a22
Sherin Olivia <SOChronicle@gmail.com>
2023-10-14 07:45:16 +0700

    chore: fixed authentication

--- dist/middleware/authenticationMiddleware.js
+++ dist/middleware/authenticationMiddleware.js
@@ -5,23 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const jwtConfig_1 = __importDefault(require("../config/jwtConfig"));
+const errorHandling_1 = require("../controller/errorHandling");
 const authenMiddleware = (req, res, next) => {
-    const authen = req.headers.authorization;
+    const authen = req.cookies.access_token;
     if (!authen) {
-        res.status(400).json({ error: "Unauthorized Access!!" });
+        return res.status(400).json({ error: "Unauthorized Access!!" });
     }
-    else {
-        const secretToken = authen.split(' ')[1];
-        try {
-            const decodedToken = jsonwebtoken_1.default.verify(secretToken, jwtConfig_1.default);
-            console.log(decodedToken, `==== User's Decoded Data`);
-            req.user = decodedToken;
-            req.role = decodedToken.role;
-            next();
-        }
-        catch (error) {
-            res.status(400).json({ error: error });
-        }
+    try {
+        const decodedToken = jsonwebtoken_1.default.verify(authen, jwtConfig_1.default);
+        console.log(decodedToken, `==== User's Decoded Data`);
+        req.user = decodedToken;
+        req.role = decodedToken.role;
+        next();
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json((0, errorHandling_1.errorHandling)(null, "Can't Authenticate!! Internal Error!"));
     }
 };
 exports.default = authenMiddleware;
